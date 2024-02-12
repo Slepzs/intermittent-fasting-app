dec329b0ba24856326f3a485d11d4fb62604df0f
Tobias Heide <slepzs@gmail.com>
2024-02-12 20:20:57 +0100

    Fix: path

--- nestjs/src/page.service.ts
+++ nestjs/src/page.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { Post, Prisma } from '../prisma/prisma-client';
+import { Post, Prisma } from '@prisma/prisma-client';
 
 @Injectable()
 export class PostService {
